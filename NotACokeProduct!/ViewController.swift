1ec83396caf50bd7c34bfcaa6eb798435296ebc9
kenneth moody <km3372@gmail.com>
2019-02-23 23:00:28 -0500

    Initial Commit

---
+++ NotACokeProduct!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotACokeProduct!
+//
+//  Created by kenneth moody on 2/23/19.
+//  Copyright © 2019 iMoody Studios LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
